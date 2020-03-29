47f20c266110c0e4b526881a9fe645b8655d88dc
Andrew Gallant <jamslam@gmail.com>
2020-03-29 19:18:57 -0400

    pkg: update brew tap to 12.0.1

--- pkg/brew/ripgrep-bin.rb
+++ pkg/brew/ripgrep-bin.rb
@@ -5,10 +5,10 @@ class RipgrepBin < Formula
 
   if OS.mac?
       url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "ebbc518ac019fc78b6429ec2e64b823c59b15a6f942436f43b9afc23c9aad9ea"
+      sha256 "59e78931a7577e76f40c937cae2782f2bbdcb40f056df5dd84247a671373525d"
   elsif OS.linux?
       url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "ab3c8b4b030102d8fcdde62d3467ea5219e65aa3b42d268e85e9d1c14624149a"
+      sha256 "2dbd6e0cef8048f26ea9f0d6b5412f117645001c5cec267b3bcd4716a64a7a7a"
   end
 
   conflicts_with "ripgrep"
