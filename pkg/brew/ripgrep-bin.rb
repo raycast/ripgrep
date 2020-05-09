bacfca174e80ff3b677e110a942e8b5e217857a1
Andrew Gallant <jamslam@gmail.com>
2020-05-09 11:42:39 -0400

    pkg: update brew tap to version 12.1.0
    This also removes Fish shell completions. See #1577 for more details.

--- pkg/brew/ripgrep-bin.rb
+++ pkg/brew/ripgrep-bin.rb
@@ -1,14 +1,14 @@
 class RipgrepBin < Formula
-  version '12.0.1'
+  version '12.1.0'
   desc "Recursively search directories for a regex pattern."
   homepage "https://github.com/BurntSushi/ripgrep"
 
   if OS.mac?
       url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "59e78931a7577e76f40c937cae2782f2bbdcb40f056df5dd84247a671373525d"
+      sha256 "974351ca7d00083ba2fad52e2f2539c8ff114119c139420f592507962ab43b75"
   elsif OS.linux?
       url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "2dbd6e0cef8048f26ea9f0d6b5412f117645001c5cec267b3bcd4716a64a7a7a"
+      sha256 "c6bba6d643b1a1f18994683e26d4d2b998b41a7a7360e63cb8ec9db8ffbf793c"
   end
 
   conflicts_with "ripgrep"
@@ -18,7 +18,6 @@ class RipgrepBin < Formula
     man1.install "doc/rg.1"
 
     bash_completion.install "complete/rg.bash"
-    fish_completion.install "complete/rg.fish"
     zsh_completion.install "complete/_rg"
   end
 end
