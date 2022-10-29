c032cda4b7afe2399109ebe42bc2d44d56de1f1b
Glenn Slotte <glenn@slotte.net>
2022-10-29 19:49:19 +0200

    ignore/types: add ReScript and ReasonML
    PR #2340

--- crates/ignore/src/default_types.rs
+++ crates/ignore/src/default_types.rs
@@ -189,7 +189,9 @@ pub const DEFAULT_TYPES: &[(&str, &[&str])] = &[
     ("racket", &["*.rkt"]),
     ("rdoc", &["*.rdoc"]),
     ("readme", &["README*", "*README"]),
+    ("reasonml", &["*.re", "*.rei"]),
     ("red", &["*.r", "*.red", "*.reds"]),
+    ("rescript", &["*.res", "*.resi"]),
     ("robot", &["*.robot"]),
     ("rst", &["*.rst"]),
     ("ruby", &[
