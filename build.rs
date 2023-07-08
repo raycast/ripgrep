db6bb21a629d5b1ec1bfe89c693b280497c9eedc
Andrew Gallant <jamslam@gmail.com>
2023-07-08 13:27:22 -0400

    windows: attempt to enable long path support for MSVC targets
    See the README and comments in the build.rs. Basically, this embeds an
    XML file that I guess is a way of setting configuration knobs on
    Windows. One of those knobs is enabling long path support. You still
    need to enable it in your registry (lol), but this will handle the other
    half of it.
    
    Fixes #364, Closes #2049

--- build.rs
+++ build.rs
@@ -48,6 +48,34 @@ fn main() {
     if let Some(rev) = git_revision_hash() {
         println!("cargo:rustc-env=RIPGREP_BUILD_GIT_HASH={}", rev);
     }
+    // Embed a Windows manifest and set some linker options. The main reason
+    // for this is to enable long path support on Windows. This still, I
+    // believe, requires enabling long path support in the registry. But if
+    // that's enabled, then this will let ripgrep use C:\... style paths that
+    // are longer than 260 characters.
+    set_windows_exe_options();
+}
+
+fn set_windows_exe_options() {
+    static MANIFEST: &str = "pkg/windows/Manifest.xml";
+
+    let Ok(target_os) = env::var("CARGO_CFG_TARGET_OS") else { return };
+    let Ok(target_env) = env::var("CARGO_CFG_TARGET_ENV") else { return };
+    if !(target_os == "windows" && target_env == "msvc") {
+        return;
+    }
+
+    let Ok(mut manifest) = env::current_dir() else { return };
+    manifest.push(MANIFEST);
+    let Some(manifest) = manifest.to_str() else { return };
+
+    println!("cargo:rerun-if-changed={}", MANIFEST);
+    // Embed the Windows application manifest file.
+    println!("cargo:rustc-link-arg-bin=rg=/MANIFEST:EMBED");
+    println!("cargo:rustc-link-arg-bin=rg=/MANIFESTINPUT:{manifest}");
+    // Turn linker warnings into errors. Helps debugging, otherwise the
+    // warnings get squashed (I believe).
+    println!("cargo:rustc-link-arg-bin=rg=/WX");
 }
 
 fn git_revision_hash() -> Option<String> {
